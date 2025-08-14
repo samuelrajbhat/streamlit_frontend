b4b9cf9bcb012d7a5d72922f9da62f1d731fb7fd
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-14 18:31:47 +0545

    home page

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
-st.set_page_config(page_title="Home Page", layout="centered")
-
+st.set_page_config(page_title="Home", layout="centered")
 st.title("🏠 Home Page")
 
-st.write(st.session_state.get("access_token")) # type: ignore
+# Protect route: require login
+if "access_token" not in st.session_state:
+    st.warning("You must log in first.")
+    st.switch_page("Login.py")
+
+st.success("Welcome! You are logged in.")
+st
