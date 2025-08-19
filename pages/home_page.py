909289cf84fda89d3d6d6ed1bf45d5e0637905a2
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-19 18:07:26 +0545

    Straming of Ai wiht table formmat

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import os
 import requests
+import pandas as pd
+import json
 
 # Load backend URL from environment variable
 BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
@@ -20,16 +22,16 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# if prompt := st.chat_input("Whats up?"):
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
+if prompt := st.chat_input("Whats up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
         
 
-user_input = st.chat_input("Enter your question!")
-if user_input:
-    with st.chat_message("user"):
-        st.write(user_input)
+# user_input = st.chat_input("Enter your question!")
+# if user_input:
+    # with st.chat_message("user"):
+    #     st.write(user_input)
     # st.write(f"User has sent the following prompt: {user_input}")
     with st.chat_message("assistant"):
         message_placeholder= st.empty()
@@ -37,7 +39,7 @@ if user_input:
         try:
             response = requests.post(
                 f"{BACKEND_URL}/ai/ask",
-                json={"messages": user_input},
+                json={"messages": prompt},
                 headers={"Content-Type": "application/json",
                         "Authorization": f"Bearer {st.session_state['access_token']}"},
                 timeout=30,
@@ -51,7 +53,17 @@ if user_input:
                     if chunk:
                         collected_text += chunk.decode()
                         message_placeholder.markdown(collected_text)
-                        # st.chat_message("assistant").write(result)  # update live
+                # Save assistant message in history
+                # check if  its a table to respond it
+                try:
+                    data= json.loads(collected_text)
+                    if isinstance(data, list) and all(isinstance(i, dict) for i in data):
+                        df = pd.DataFrame(data)
+                        message_placeholder.dataframe(df)
+                        collected_text= df.to_markdown()
+                except Exception:
+                    pass
+                st.session_state.messages.append({"role": "assistant", "content": collected_text})
             else:
                 st.error(f'Error: {response.status_code} - {response.text}')
 
