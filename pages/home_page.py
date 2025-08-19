c3bac40f1fd0dde0e1467b9ada32d4872745df0a
Samuel Raj Bhat <samuelrajbhat5@gmail.com>
2025-08-19 17:12:39 +0545

    Straming of Ai message

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,34 +11,55 @@ st.title("🏠 Home Page")
 # Protect route: require login
 if "access_token" not in st.session_state:
     st.warning("You must log in first.")
-    st.switch_page("Login.py")
+    st.switch_page("app.py")
 
-st.success("Welcome! You are logged in.")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# if prompt := st.chat_input("Whats up?"):
+#     st.session_state.messages.append({"role": "user", "content": prompt})
+#     with st.chat_message("user"):
+#         st.markdown(prompt)
+        
+
 user_input = st.chat_input("Enter your question!")
 if user_input:
-    try:
-        response = requests.post(
-            f"{BACKEND_URL}/ai/ask",
-            json={"messages": user_input},
-            headers={"Content-Type": "application/json",
-                     "Authorization": f"Bearer {st.session_state['access_token']}"},
-            timeout=5,
-            stream=True
-        )
-        if response.status_code == 200:
-            result = ""
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    result += chunk.decode()
-                    st.chat_message("assistant").write(result)  # update live
-        else:
-            st.error(f'Error: {response.status_code} - {response.text}')
+    with st.chat_message("user"):
+        st.write(user_input)
+    # st.write(f"User has sent the following prompt: {user_input}")
+    with st.chat_message("assistant"):
+        message_placeholder= st.empty()
+        collected_text = ""
+        try:
+            response = requests.post(
+                f"{BACKEND_URL}/ai/ask",
+                json={"messages": user_input},
+                headers={"Content-Type": "application/json",
+                        "Authorization": f"Bearer {st.session_state['access_token']}"},
+                timeout=30,
+                stream=True
+            )
+            
+        
+            if response.status_code == 200:
+                
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        collected_text += chunk.decode()
+                        message_placeholder.markdown(collected_text)
+                        # st.chat_message("assistant").write(result)  # update live
+            else:
+                st.error(f'Error: {response.status_code} - {response.text}')
 
         # elif response.status_code == 401:
         #     st.error("Invalid username or password.")
         # else:
         #     st.error(f'Error: {response.status_code} - {response.text}')
-    except requests.exceptions.RequestException as e:
-        st.error(f"Network Error: {e}")
+        except requests.exceptions.RequestException as e:
+            st.error(f"Network Error: {e}")
 
 
